a48ec647755b9607698ad1ddd87fc21f87e94fa0
Constantin Rack <constantin@rack.li>
2015-05-10 10:50:47 +0200

    Add mix task to fetch latest IP lists. Added IP lists.

---
+++ lib/mix/tasks/update_ips.ex
@@ -0,0 +1,21 @@
+defmodule Mix.Tasks.Update.Ips do
+  use Mix.Task
+
+  @urls [
+    "https://www.cloudflare.com/ips-v4",
+    "https://www.cloudflare.com/ips-v6"
+  ]
+
+  @shortdoc "Fetches the latest IPs from Cloudflare."
+
+  def run(_) do
+    System.cmd("rm", ["ips-v4", "ips-v6"])
+    for url <- @urls, do: fetch url
+  end
+
+  defp fetch(url) do
+    IO.puts "Fetching #{url}"
+    System.cmd("wget", ["--quiet", url])
+  end
+
+end
