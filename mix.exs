6874df14288e6034927af2b0a8ebc5f3253f0241
Constantin Rack <constantin@rack.li>
2015-11-15 17:14:33 +0100

    Problem: mix.exs file was not alphabetically sorted
    Solution: sort it

--- mix.exs
+++ mix.exs
@@ -4,41 +4,37 @@ defmodule Plug.Cloudflare.Mixfile do
   def project do
     [
       app: :plug_cloudflare,
-      version: "1.2.1",
-      elixir: ">= 1.0.2",
       build_embedded: Mix.env == :prod,
+      deps: [
+        {:cidr, ">= 0.3.0"},
+        {:plug, ">= 0.11.0"}
+      ],
+      description: """
+      Convert CloudFlare's CF-Connecting-IP header to Plug.Conn's remote_ip field.
+      """,
+      elixir: ">= 1.0.2",
+      package: %{
+        maintainers: ["Constantin Rack"],
+        files: [
+          "ips-v4",
+          "ips-v6",
+          "lib",
+          "mix.exs",
+          "LICENSE",
+          "README.md"
+        ],
+        licenses: ["Apache License 2.0"],
+        links: %{
+          "GitHub" => "https://github.com/c-rack/plug_cloudflare"
+        }
+      },
       start_permanent: Mix.env == :prod,
-      deps: deps,
-      description: "Parses CloudFlare's CF-Connecting-IP header into Plug.Conn's remote_ip field.",
-      package: package,
+      version: "1.2.1"
     ]
   end
 
   def application do
-    [applications: [:logger]]
-  end
-
-  defp deps do
-    [
-      {:cidr, ">= 0.3.0"},
-      {:plug, ">= 0.11.0"}
-    ]
-  end
-
-  defp package do
-    %{
-      maintainers: ["Constantin Rack"],
-      files: [
-        "lib",
-        "mix.exs",
-        "ips-v4",
-        "ips-v6",
-        "README.md",
-        "LICENSE"
-      ],
-      licenses: ["Apache License 2.0"],
-      links: %{"GitHub" => "https://github.com/c-rack/plug_cloudflare"}
-    }
+    [applications: []]
   end
 
 end
